668f8b980a7c64a862a3b42182fb23bca2ad6ca0
Sailor Socks <socksster@gmail.com>
2015-12-14 20:54:24 -0500

    Convert all tabs to spaces; add comments where needed

--- media.py
+++ media.py
@@ -1,14 +1,19 @@
 import webbrowser
 
+
+# Create class "Movie" with attributes:
+#   title, data, director, tagline, poster & trailer
 class Movie():
-	def __init__(self, movie_title, movie_date, movie_director, movie_tagline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.date = movie_date
-		self.director = movie_director
-		self.tagline = movie_tagline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-		
-		
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    def __init__(self, movie_title, movie_date, movie_director, movie_tagline,
+                 poster_image, trailer_youtube):
+        self.title = movie_title
+        self.date = movie_date
+        self.director = movie_director
+        self.tagline = movie_tagline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+# Define show_trailer function to open a browser and show trailer for each
+#    instance of class "Movie"
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
